9ef42071f0facddedbfefad3e7b36b0e4adabc6b
wiseyb <107757999+wiseyb@users.noreply.github.com>
2026-06-29 16:58:53 +0100

    Upload app.py from ZIP

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import font as tkFont
+from collections import Counter
 import sys
 import os
 import shutil
@@ -10,16 +11,34 @@ import tempfile
 import filecmp
 
 
+def rgb_to_hex(rgb):
+    return '#%02x%02x%02x' % rgb
+
+
+def get_bg_color(img, step=10):
+    pixels = []
+
+    width = img.width()
+    height = img.height()
+
+    for x in range(0, width, step):
+        for y in range(0, height, step):
+            pixels.append(img.get(x, y))  # returns (r, g, b)
+
+    most_common = Counter(pixels).most_common(1)[0][0]
+    return rgb_to_hex(most_common)
+
+
 class RequirementsError(Exception):
     '''Missing required contents'''
 
 
-
+bgimagepath='./ATT/LOGO-NEW.png'
 
 syncerror=False
 missingReq=False
 
-required=['./py','./py/camp_edit.py','./py/game.py','./ATT/BACK.png']
+required=['./py','./py/camp_edit.py','./py/game.py',bgimagepath]
 
 #Add local path /py to modules
 sys.path.append('./py')
@@ -84,7 +103,7 @@ try:
         sync(local_dir,repo_root,delete_ext=True)
 except Exception as e:
     syncerror=True
-    if str(e) == "<urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: Basic Constraints of CA cert not marked critical (_ssl.c:1032)>":
+    if "<urlopen error [SSL: CERTIFICATE_VERIFY_FAILED] certificate verify failed: Basic Constraints of CA cert not marked critical (_ssl.c:" in str(e):
         exp='SSL certificate verification failed'
     else:
         exp=e
@@ -110,23 +129,30 @@ Quitting...''')
             os._exit(0)
         
 #Init window
+
+name='POLYMORPHISM'
+
 root=Tk()
-root.title('POLYMORPHISM')
-bgr=PhotoImage(file='./ATT/BACK.png')
-w=bgr.width()
-h=bgr.height()
+root.title(name)
+bgr=PhotoImage(file=bgimagepath)
+w=bgr.width()+400
+h=bgr.height()+50
 ws=root.winfo_screenwidth()
 hs=root.winfo_screenheight()
 x=int((ws/2)-(w/2))
 y=int((hs/2)-(h/2))
 dim=f'{w}x{h}+{x}+{y}'
 root.geometry(dim)
-l1=Label(root,image=bgr)
-l1.place(x=-2,y=-2)
+root.configure(bg=get_bg_color(bgr))
+bp=Label(root,image=bgr,bd=0)
+
+
+
+#determine hover colours for buttons
 
 hexv=['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 
-def ls(a,f):
+def ls(a,f): # Linear search algorithm
     found=False
     for i in range(len(a)):
         if a[i]==f:
@@ -143,7 +169,7 @@ baf='#000'
 
 ho='#350000'
 
-bbg='#0a0b0a'
+bbg=get_bg_color(bgr)
 
 bfg='#ff0000'
 
@@ -156,25 +182,51 @@ for c in range(len(bbg)):
         nv=hexv[nvi]
         hoverc+=nv
 
+
+# Functions requrired for buttons and keybinds
 def deny(e):
     print('Nuh-uh')
 
 
 def hover(e):
     e.widget['background']=hoverc
-    e.widget['height']=2
 
 def un_hover(e):
     e.widget['background']=bbg
-    e.widget['height']=1
 
+def home():
+    b1.configure(text = '''Close
+[Q]''',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=2,width=bwid,command = close,font=('BankGothic Lt BT', 12))
+    b2.configure(text = '''Play Game
+[SPACE]''',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=2,width=bwid,command=game,font=('BankGothic Lt BT', 12))
+    b3.configure(text = '''Campaign Editor
+[E]''',activebackground=bab,activeforeground=baf,bd=0,bg=bbg,fg=bfg,height=2,width=bwid,command=camp,font=('BankGothic Lt BT', 12))
+    bp.place(relx=0.5, rely=0.5, anchor='center')
+    t1.place(relx=0.5, rely=0.5, anchor='center')
+    t2.place(relx=0.5, rely=0.6, anchor='center')
+    bf.place(relx=0.5, rely=0.75, anchor='center')
+    b1.pack(side='left', padx=30)
+    b2.pack(side='left', padx=30)
+    b3.pack(side='left', padx=30)
 
 def close():
     root.destroy()
     import os
     os._exit(0)
 def game():
-    print('Play')
+    bp.place_forget()
+    t1.place_forget()
+    t2.place_forget()
+    b1.configure(text='''Home
+[Q]''', command=home)
+    b2.configure(text='''Attack
+[SPACE]''')
+    b3.configure(text='''Spell
+[E]''')
+    bf.place(relx=0.5, rely=0.7, anchor='center')
+    b1.pack(side='left', padx=40)
+    b2.pack(side='left', padx=40)
+    b3.pack(side='left', padx=40)
 def camp():
     print('Campaign Editor')
 
@@ -187,7 +239,12 @@ def trigger_button_release(btn):
     btn.event_generate("<ButtonRelease-1>")
     btn.event_generate("<Leave>")
 
+#Define Labels
+t1=Label(root,text=name,fg=bfg,bg=bbg,font=('BankGothic Lt BT', 40))
+t2=Label(root,text='Shape your own reality',fg=bfg,bg=bbg,font=('BankGothic Lt BT', 20))
 
+
+#Determine button widths and padding
     
 bwid=13
 wid=w
@@ -195,32 +252,36 @@ gap=(wid-(3*bwid))/10
 fx=gap/2
 bx=gap/2
 
+
+bf=Frame(root,bg=bbg)
+
 #Define Buttons
 
-b1 = Button(root, text = 'Close',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=1,width=bwid,command = close,font=('BankGothic Lt BT', 12))
-b2= Button(root, text = 'Play Game',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=1,width=bwid,command=game,font=('BankGothic Lt BT', 12))
-b3= Button(root, text = 'Campaign Editor',activebackground=bab,activeforeground=baf,bd=0,bg=bbg,fg=bfg,height=1,width=bwid,command=camp,font=('BankGothic Lt BT', 12))
-py=(300,0)
+b1 = Button(bf, text = '''Close
+[Q]''',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=2,width=bwid,command = close,font=('BankGothic Lt BT', 12))
+b2= Button(bf, text = '''Play Game
+[SPACE]''',bd=0,activebackground=bab,activeforeground=baf,bg=bbg,fg=bfg,height=2,width=bwid,command=game,font=('BankGothic Lt BT', 12))
+b3= Button(bf, text = '''Campaign Editor
+[E]''',activebackground=bab,activeforeground=baf,bd=0,bg=bbg,fg=bfg,height=2,width=bwid,command=camp,font=('BankGothic Lt BT', 12))
+py=(0,40)
 px=(fx,bx)
 
 #Render Homescreeen
+home()
 
-b1.pack(side = 'left',pady=py,padx=px)
-b2.pack(side = 'left',pady=py,padx=px)
-b3.pack(side = 'left',pady=py,padx=px)
 
 # Custom Keyboard Shortcuts
 
 
-# Play Game (key 1)
+# Play Game (space key)
 root.bind("<KeyPress-space>", lambda e: trigger_button_press(b2))
 root.bind("<KeyRelease-space>", lambda e: trigger_button_release(b2))
 
-# Close (key 2)
+# Close (q key)
 root.bind("<KeyPress-q>", lambda e: trigger_button_press(b1))
 root.bind("<KeyRelease-q>", lambda e: trigger_button_release(b1))
 
-# Campaign (key 3)
+# Campaign (e key)
 root.bind("<KeyPress-e>", lambda e: trigger_button_press(b3))
 root.bind("<KeyRelease-e>", lambda e: trigger_button_release(b3))
 
